7a46f622f49feed51e169305f6954dd6ac9b764a
Chris Ridout <155513841+13archae@users.noreply.github.com>
2024-09-17 07:23:58 -0600

    Update holy-grail.test.js

--- holy-grail.test.js
+++ holy-grail.test.js
@@ -1,6 +1,6 @@
 const HOLY_GRAIL = require("./index.js");
 
-describe("holy-grail", () => {
+describe("holy-grail-app", () => {
   test("not null", async () => {
     await expect(HOLY_GRAIL.read()).not.toEqual(null);
   });
