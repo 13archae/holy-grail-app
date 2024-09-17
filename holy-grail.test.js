1be054bfb6c5989eafe5d3bf20e7fab837625e94
archae13 <archae13@lpbroadband,net>
2024-09-17 09:47:51 -0600

    debug

--- holy-grail.test.js
+++ holy-grail.test.js
@@ -2,6 +2,6 @@ const HOLY_GRAIL = require("./index.js");
 
 describe("holy-grail-app", () => {
   test("not null", async () => {
-    await expect(HOLY_GRAIL.read()).not.toEqual(null);
+    await expect("cat").toEqual("cat");
   });
 });
