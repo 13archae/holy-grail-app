78a5b3999cccff92bde9184d13abd38b1eae7009
archae13 <archae13@lpbroadband,net>
2024-09-17 10:11:45 -0600

    post-console.log()

--- holy-grail.test.js
+++ holy-grail.test.js
@@ -1,6 +1,6 @@
 const HOLY_GRAIL = require("./index.js");
 
-describe("holy-grail-app", () => {
+describe("holy-grail-app", async () => {
   test("not null", async () => {
     await expect("cat").toEqual("cat");
   });
--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const REDIS_CLIENT = createClient({
   url: "redis://default:0VAMxueUaV9KqJvK1zjAekMOwVnKX41p@redis-10925.c8.us-east-1-4.ec2.redns.redis-cloud.com:10925",
   connectTimeout: 10000,
 });
-console.log("REDIS_CLIENT:  ", REDIS_CLIENT);
+//console.log("REDIS_CLIENT:  ", REDIS_CLIENT);
 
 while (true) {
   if (!REDIS_CLIENT.isOpen) {
@@ -23,10 +23,12 @@ while (true) {
   }
 }
 
-REDIS_CLIENT.on("error", (err) => console.log("redis client error:  ", err));
+REDIS_CLIENT.on("error", (err) => {
+  throw err
+}));
 
 REDIS_CLIENT.on("connect", () => {
-  console.log("Connected!");
+  //console.log("Connected!");
 });
 
 // init values
@@ -40,10 +42,10 @@ const json = {
 
 REDIS_CLIENT.hSet("tracking_obj", "screen_data", JSON.stringify(json))
   .then((value) => {
-    console.log("init DB: ", value);
+    //console.log("init DB: ", value);
   })
   .catch((e) => {
-    console.log("error in init set", e);
+    //console.log("error in init set", e);
   });
 
 // serve static files from public directory
@@ -58,7 +60,7 @@ function data() {
   return new Promise((resolve, reject) => {
     REDIS_CLIENT.hGet("tracking_obj", "screen_data")
       .then((data_string) => {
-        console.log(`hGet Success:  ${data_string}`);
+        //console.log(`hGet Success:  ${data_string}`);
 
         //const value_obj = JSON.parse(value);
 
@@ -71,12 +73,12 @@ function data() {
         // };
 
         //data_string = JSON.stringify(value);
-        console.log("data() data_string : ", data_string);
+        //console.log("data() data_string : ", data_string);
 
         resolve(data_string);
       })
       .catch((err) => {
-        console.log("data() error", err);
+        //console.log("data() error", err);
         reject(null);
       });
   });
@@ -86,14 +88,14 @@ function data() {
  * DATA Endpoint
  */
 app.get("/data", (req, res) => {
-  console.log("*** in /data endpoint ***");
+  //console.log("*** in /data endpoint ***");
   data()
     .then((outData) => {
-      console.log(outData);
+      //console.log(outData);
       res.send(outData);
     })
     .catch((err) => {
-      console.log("app.get() /data error", err);
+      //console.log("app.get() /data error", err);
       throw err;
     });
 });
@@ -102,13 +104,13 @@ app.get("/data", (req, res) => {
  *  UPDATE Endpoint
  */
 app.get("/update/:key/:value", (req, res) => {
-  console.log(`*** in /update endpoint ***`);
+  //console.log(`*** in /update endpoint ***`);
   const req_key = req.params.key;
   const req_value = Number(req.params.value);
-  console.log(`req_key: ${req_key}  ::  req_value: ${req_value}`);
+  //console.log(`req_key: ${req_key}  ::  req_value: ${req_value}`);
   REDIS_CLIENT.hGet("tracking_obj", "screen_data")
     .then((str_JSON) => {
-      console.log("/update hGet() : Object Found: ", str_JSON);
+      //console.log("/update hGet() : Object Found: ", str_JSON);
 
       if (!str_JSON) {
         console.log("/update hGet() str_JSON error: ", err);
@@ -127,36 +129,36 @@ app.get("/update/:key/:value", (req, res) => {
 
       REDIS_CLIENT.hSet("tracking_obj", "screen_data", track_obj_JSON)
         .then((response) => {
-          console.log("hSet() in /update SUCCESS : values set: ", response);
+          //console.log("hSet() in /update SUCCESS : values set: ", response);
         })
         .catch((err) => {
-          console.log("Failed hSet() in /update", err);
+          //console.log("Failed hSet() in /update", err);
         });
 
       // return data to client
       data()
         .then((in_data) => {
-          console.log("in_data", in_data);
+          //console.log("in_data", in_data);
 
           res.send(in_data);
         })
         .catch((err) => {
-          console.log("/update data() call error:  ", err);
+          //console.log("/update data() call error:  ", err);
         });
     })
     .catch((err) => {
       if (err) {
-        console.log("/update hGet() error: ", err);
+        //console.log("/update hGet() error: ", err);
         res.status(404).send("/update hGet: ", err);
       }
     });
 });
 
 app.listen(3000, () => {
-  console.log("Running on 3000");
+  //console.log("Running on 3000");
 });
 
 process.on("exit", function () {
   quit();
-  console.log("Session End:  ", Date.now());
+  //console.log("Session End:  ", Date.now());
 });
